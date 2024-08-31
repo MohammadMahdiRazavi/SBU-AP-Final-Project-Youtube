9a8d93691c8e7c7730c8ffd5b6f08e829a4d3f47
MohammadMahdiRazavi <mohammed.razavi1384@gmail.com>
2024-08-31 02:03:16 -0700

    AddVideoToUserPlaylist class completed

---
+++ src/main/java/finalproject/youtube/server/requests/AddVideoToUserPlaylist.java
@@ -0,0 +1,32 @@
+package finalproject.youtube.server.requests;
+
+import finalproject.youtube.database.DatabaseManger;
+import finalproject.youtube.server.Request;
+import org.json.JSONObject;
+
+public class AddVideoToUserPlaylist implements Request {
+    private JSONObject jsonResponse;
+
+    @Override
+    public void conductRequest(JSONObject request) {
+        String video_id = request.getString("video_id");
+        String playlist_id = request.getString("playlist_id");
+        int SQLResponse = DatabaseManger.addVideoToUserPlaylist(video_id, playlist_id);
+        makeJson(SQLResponse);
+    }
+
+    @Override
+    public JSONObject getJsonResponse() {
+        return jsonResponse;
+    }
+
+    @Override
+    public void makeJson(int res) {
+        jsonResponse = new JSONObject();
+        if (res == 1){
+            jsonResponse.put("response", "Video added successfully");
+        } else if (res == -1) {
+            jsonResponse.put("response", "Video add failed");
+        }
+    }
+}
